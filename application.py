8716b5dcf017f56efea422f8655a32922718465f
iHakan <ihakan@yaani.com>
2020-12-29 04:40:40 +0300

    import model.py

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-import model
+from model import *
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
